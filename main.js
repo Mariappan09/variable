f9cfb2a2e6ac64ba64ded02913798226d3926015
Your Name <youamariappan2001@example.com>
2023-03-13 21:41:15 +0530

    add

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log("---var---")
+var x=10;
+var y=5;
+var z=x+y;
+console.log(z);
+var m="mari";n=12;
+var o=m+n;
+console.log(o);
+
+var x="20";
+var y=5;
+var z=x+y;
+console.log(z);
+
+
+console.log("---let---")
+let a=6;
+let b=4;
+let c=a+b;
+console.log(c);
+
+
+console.log("---const---")
+const price1=9;
+const price2=10;
+const total=price1+price2;
+console.log(price1+price2);
+
+a=11,b=a-a++;
+console.log(a);
+console.log(b);
+console.log(a++);
+console.log(--a);
+console.log(++a);
+console.log(a--);
+
+
